0de022ea8b0ed263e4bd6e37e0884f99cce759d6
max.karavaev <daily.spb@yandex.ru>
2021-12-09 21:42:36 +0300

    Initial commit

---
+++ src/main/java/com/naissur/Main.java
@@ -0,0 +1,10 @@
+package com.naissur;
+
+/**
+ * @author Max Karavaev (daily.spb@yandex.ru)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
